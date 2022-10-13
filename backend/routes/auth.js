91d3f6c256fba3f23f50860fd2ba2a466870baca
DerzoGit <journetalexandre.contact@gmail.com>
2022-10-13 13:25:32 +0200

    clear auth routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,5 @@
 const express = require("express")
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
-
-const db = require("../models/index")
+const authCtrl = require("../controllers/auth")
 
 let router = express.Router()
 
@@ -12,37 +9,6 @@ router.use((req, res, next) => {
     next()
 })
 
-router.post("/login", (req, res) => {
-    const { email, password } = req.body
-
-    if(!email || !password) {
-        return res.status(400).json({ message: "Bad email or password" })
-    }
-
-    db.User.findOne({ where: { email: email }, raw: true })
-        .then(user => {
-            if(user === null) {
-                return res.status(401).json({ message: "This account doesn't exist" })
-            }
-
-            bcrypt.compare(password, user.password)
-                .then(test => {
-                    if(!test) {
-                        return res.status(401).json({ message: "Wrong password" })
-                    }
-
-                    const token = jwt.sign({
-                        id: user.id,
-                        nom: user.nom,
-                        prenom: user.prenom,
-                        email: user.email
-                    }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_DURING})
-
-                    return res.json({ access_token: token })
-                })
-                .catch(err => res.status(500).json({ message: "Login process failed", error: err }))
-        })
-        .catch(err => res.status(500).json({ message: "Database Error", error: err }))
-})
+router.post("/login", authCtrl.login)
 
 module.exports = router
