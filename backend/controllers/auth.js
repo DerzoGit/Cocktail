281e1cba09feb052da09135828b690b5a947f97a
DerzoGit <journetalexandre.contact@gmail.com>
2022-10-13 13:55:08 +0200

    clear auth ctrls with try/catch

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -2,35 +2,37 @@ const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 const db = require("../models/index")
 
-exports.login = (req, res) => {
+exports.login = async (req, res) => {
     const { email, password } = req.body
 
     if(!email || !password) {
         return res.status(400).json({ message: "Bad email or password" })
     }
 
-    db.User.findOne({ where: { email: email }, raw: true })
-        .then(user => {
-            if(user === null) {
-                return res.status(401).json({ message: "This account doesn't exist" })
-            }
+    try {
+        let user = await db.User.findOne({ where: { email: email }, raw: true })
+        if(user === null) {
+            return res.status(401).json({ message: "This account doesn't exist" })
+        }
 
-            bcrypt.compare(password, user.password)
-                .then(test => {
-                    if(!test) {
-                        return res.status(401).json({ message: "Wrong password" })
-                    }
+        let test = await bcrypt.compare(password, user.password)
+        if(!test) {
+            return res.status(401).json({ message: "Wrong password" })
+        }
 
-                    const token = jwt.sign({
-                        id: user.id,
-                        nom: user.nom,
-                        prenom: user.prenom,
-                        email: user.email
-                    }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_DURING})
+        const token = jwt.sign({
+            id: user.id,
+            nom: user.nom,
+            prenom: user.prenom,
+            email: user.email
+        }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_DURING})
 
-                    return res.json({ access_token: token })
-                })
-                .catch(err => res.status(500).json({ message: "Login process failed", error: err }))
-        })
-        .catch(err => res.status(500).json({ message: "Database Error", error: err }))
+        return res.json({ access_token: token })
+    } catch(err) {
+        if(err.name == "SequelizeDatabaseError") {
+            res.status(500).json({ message: "Database Error", error: err })
+        }
+        res.status(500).json({ message: "Login process failed", error: err })
+        
+    }
 }
