dc99193f293d02704a73a31fadca36b74ae6d949
DerzoGit <journetalexandre.contact@gmail.com>
2022-10-18 11:34:28 +0200

    user error management

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,108 +1,119 @@
 const bcrypt = require("bcrypt")
-
 const db = require("../models/index")
+const { RequestError, UserError } = require("../middleware/customError")
 
-exports.getAllUsers = (req, res) => {
+exports.getAllUsers = (req, res, next) => {
     db.User.findAll()
         .then(users => res.json({ data: users }))
-        .catch(err => res.status(500).json({ message: "Database Error", error: err }))
+        .catch(err => next(err))
 }
 
-exports.getUser = (req, res) => {
-    let userId = parseInt(req.params.id)
+exports.getUser = async (req, res, next) => {
+    try {
+        let userId = parseInt(req.params.id)
 
-    if(!userId) {
-        return res.json(400).json({ message: "Missing parameter" })
-    }
-
-    db.User.findOne({ where: { id: userId }, raw: true })
-    .then(user => {
-        if((user == null)){
-            return res.status(404).json({ message: "This user doesn't exist !" })
+        if(!userId) {
+            throw new UserError("Missing Parameter")
         }
-        return res.json({ data: user })
-    })
-    .catch(err => res.status(500).json({ message: "Database error", error: err }))
-}
-
-exports.addUser = (req, res) => {
-    const { nom, prenom, pseudo, email, password } = req.body
-
-    if(!nom || !prenom || !pseudo || !email || !password) {
-        return res.status(400).json({ message: "Missing data" })
-    }
-
-    db.User.findOne({ where: { email: email }, raw: true })
-        .then(user => {
-            if(user !== null) {
-                return res.status(409).json({ message: `The user ${nom} already exist` })
+    
+        let user = await db.User.findOne({ where: { id: userId }, raw: true })
+            if((user == null)){
+                throw new UserError("This user doesn't exist !", 0)
             }
-
-            bcrypt.hash(password, parseInt(process.env.BCRYPT_SALT_ROUND))
-                .then(hash => {
-                    req.body.password = hash
-
-                    db.User.create(req.body)
-                        .then(user => res.json({ message: "User created", data:user }))
-                        .catch(err => res.status(500).json({ message: "Database error", error: err }))
-                })
-                .catch(err => res.status(500).json({ message: "Hash process error", error: err }))
-        })
-        .catch(err => res.status(500).json({ message: "Database error", error: err }))
+            return res.json({ data: user })
+    } catch(err) {
+        next(err)
+    }
 }
 
-exports.updateUser = (req, res) => {
-    let userId = parseInt(req.params.id)
+exports.addUser = async (req, res, next) => {
+    try {
+        const { nom, prenom, pseudo, email, password } = req.body
 
-    if(!userId) {
-        return res.status(400).json({ message: "Missing parameter" })
+        if(!nom || !prenom || !pseudo || !email || !password) {
+            throw new UserError("Missing paremeter")
+        }
+    
+        let user = await db.User.findOne({ where: { email: email }, raw: true })
+                if(user !== null) {
+                    throw new UserError(`The user ${nom} already exist`, 1)
+                }
+    
+                let hash = await bcrypt.hash(password, parseInt(process.env.BCRYPT_SALT_ROUND))
+                req.body.password = hash
+    
+                user = await db.User.create(req.body)
+                return res.json({ message: "User created", data:user })
+    } catch(err) {
+        next(err)
     }
-
-    db.User.findOne({ where: { id: userId }, raw: true })
-        .then(user => {
-            if(user === null) {
-                return res.status(404).json({ message: "This user doesn't exist" })
-            }
-
-            db.User.update(req.body, { where: { id: userId } })
-                .then(user => res.status(200).json({ message: "User updated" }))
-                .catch(err => res.status(500).json({ message: "Database error", error: err }))
-        })
-        .catch(err => res.status(500).json({ message: "Database error", error: err }))
 }
 
-exports.untrashUser = (req, res) => {
-    let userId = parseInt(req.params.id)
+exports.updateUser = async (req, res, next) => {
+    try {
+        let userId = parseInt(req.params.id)
 
-    if(!userId) {
-        return res.status(400).json({ message: "Missing parameter" })
+        if(!userId) {
+            throw new UserError("Missing parameter")
+        }
+    
+        let user = await db.User.findOne({ where: { id: userId }, raw: true })
+                if(user === null) {
+                    throw new UserError("This user doesn't exist", 0)
+                }
+    
+                user = await db.User.update(req.body, { where: { id: userId } })
+                return res.status(200).json({ message: "User updated" })
+
+    } catch(err) {
+        next(err)
     }
-
-    db.User.restore({ where: { id: userId } })
-        .then(() => res.status(204).json({}))
-        .catch(err => res.status(500).json( { message: "Database error", error: err }))
 }
 
-exports.trashUser = (req, res) => {
-    let userId = parseInt(req.params.id)
+exports.untrashUser = async (req, res, next) => {
+    try {
+        let userId = parseInt(req.params.id)
 
-    if(!userId) {
-        return res.status(400).json({ message: "Missing parameter" })
+        if(!userId) {
+            throw new UserError("Missing parameter")
+        }
+    
+        await db.User.restore({ where: { id: userId } })
+        return res.status(204).json({})
+            
+    } catch(err) {
+        next(err)
     }
-
-    db.User.destroy({ where: { id: userId } })
-        .then(() => res.status(204).json({}))
-        .catch(err => res.status(500).json({ message: "Database error", error: err }))
 }
 
-exports.deleteUser = (req, res) => {
-    let userId = parseInt(req.params.id)
+exports.trashUser = async (req, res, next) => {
+    try {
+        let userId = parseInt(req.params.id)
 
-    if(!userId) {
-        return res.status(400).json({ message: "Missing parameter" })
+        if(!userId) {
+            throw new UserError("Missing parameter")
+        }
+    
+        await db.User.destroy({ where: { id: userId } })
+        return res.status(204).json({})
+
+    } catch(err) {
+        next(err)
     }
+}
 
-    db.User.destroy({ where: { id: userId }, force: true })
-        .then(() => res.status(204).json({}))
-        .catch(err => res.status(500).json({ message: "Database error", error: err }))
+exports.deleteUser = async (req, res, next) => {
+    try {
+        let userId = parseInt(req.params.id)
+
+        if(!userId) {
+            throw new UserError("Missing parameter")
+        }
+    
+        await db.User.destroy({ where: { id: userId }, force: true })
+        return res.status(204).json({})
+        
+    } catch(err) {
+        next(err)
+    }
 }
