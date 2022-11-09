b4be666bae70716dff9eba3d41bafb4fd0e7c0cd
DerzoGit <journetalexandre.contact@gmail.com>
2022-11-09 22:20:32 +0100

    change db & clear

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,5 @@
-// const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
-const db = require("../models/index")
+const db = require("../db.config")
 const { AuthenticationError } = require("../middleware/customError")
 
 exports.login = async (req, res, next) => {
@@ -16,7 +15,6 @@ exports.login = async (req, res, next) => {
             throw new AuthenticationError("This account doesn't exist", 1)
         }
 
-        // let test = await bcrypt.compare(password, user.password)
         let test = await db.User.checkPassword(password, user.password)
         if(!test) {
             throw new AuthenticationError("Wrong password", 2)
