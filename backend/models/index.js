dfe2d490cb3e6eccf6ac44a1da332e8b9092efc9
DerzoGit <journetalexandre.contact@gmail.com>
2022-11-09 22:37:49 +0100

    del index models

--- backend/models/index.js
+++
@@ -1,9 +0,0 @@
-const User = require("./user")
-const Cocktail = require("./cocktail")
-
-User.hasMany(Cocktail, { foreignKey: "userId", onDelete: "cascade" })
-Cocktail.belongsTo(User, { foreignKey: "userId" })
-
-module.exports = {
-    User, Cocktail
-}
