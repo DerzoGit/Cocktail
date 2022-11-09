4f62d3aa67b767df81892746e92c33f520442375
DerzoGit <journetalexandre.contact@gmail.com>
2022-11-09 22:22:06 +0100

    change model exports

--- backend/models/cocktail.js
+++ backend/models/cocktail.js
@@ -1,23 +1,13 @@
-const { Model, DataTypes } = require("sequelize")
-const sequelize = require("../db.config")
+const { DataTypes } = require("sequelize")
 
-class Cocktail extends Model {}
-
-Cocktail.init({
+module.exports = (sequelize) => {
+    return Cocktail = sequelize.define("Cocktail", {
         id: { type: DataTypes.INTEGER(10), primaryKey: true, autoIncrement: true },
         userId: { type: DataTypes.INTEGER(10), allowNull: false },
         nom: { type: DataTypes.STRING(100), defaultValue: "", allowNull: false },
         description: { type: DataTypes.TEXT, defaultValue: "", allowNull: false },
         recette: { type: DataTypes.TEXT, defaultValue: "", allowNull: false }
-    }, {
-        sequelize,
-        modelName: "Cocktail",
-        tableName: "cocktails",
+        }, {
         paranoid: true // Soft delete
     })
-
-// Cocktail.sync()
-// Cocktail.sync({ force: true })
-// Cocktail.sync({ alter: true })
-
-    module.exports = Cocktail;
+}
