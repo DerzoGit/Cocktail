a8b80988df7b3b58e38615ee7411011657b9f59e
DerzoGit <journetalexandre.contact@gmail.com>
2022-11-15 09:15:43 +0100

    create publics views & layout

---
+++ frontend/src/views/public/Layout.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="public">
+        Public Layout
+        <router-view/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PublicLayout"
+}
+</script>
+
+<style>
+    .public {
+        border: solid 1px red
+    }
+</style>
