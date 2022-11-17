010d8030ce47e88eeadb4b05e623294cb8cccbf6
DerzoGit <journetalexandre.contact@gmail.com>
2022-11-17 15:01:50 +0100

    index admin views

---
+++ frontend/src/views/admin/index.js
@@ -0,0 +1,15 @@
+import AdminLayout from "./Layout"
+import Dashboard from "./Dashboard"
+
+import UserIndex from "./users/UserIndex"
+import UserEdit from "./users/UserEdit"
+import UserAdd from "./users/UserAdd"
+
+import CocktailIndex from "./cocktails/CocktailIndex"
+import CocktailEdit from "./cocktails/CocktailEdit"
+
+export {
+    AdminLayout, Dashboard, 
+    UserIndex, UserEdit, UserAdd, 
+    CocktailIndex, CocktailEdit
+}
