20591d685d41a2b578cf4218f8160463945a3be1
DerzoGit <journetalexandre.contact@gmail.com>
2022-11-17 15:01:40 +0100

    index public views

---
+++ frontend/src/views/public/index.js
@@ -0,0 +1,8 @@
+import PublicLayout from "./Layout"
+import Home from "./Home"
+import Cocktail from "./Cocktail"
+import Contact from "./Contact"
+
+export {
+    PublicLayout, Home, Cocktail, Contact
+}
