b50410e1c1c245beb6cdd1e243581818daa01e18
DerzoGit <journetalexandre.contact@gmail.com>
2022-11-17 15:02:43 +0100

    admin userindex

--- frontend/src/views/admin/users/UserIndex.vue
+++ frontend/src/views/admin/users/UserIndex.vue
@@ -5,9 +5,15 @@
 </template>
 
 <script>
+import { userService } from "@/_services/user.service"
 
 export default {
-    name: "UserIndex"
+    name: "UserIndex",
+    mounted() {
+        userService.getAllUsers()
+            .then(res => console.log(res.data.data))
+            .catch(err => console.log(err))
+    }
 }
 </script>
 
