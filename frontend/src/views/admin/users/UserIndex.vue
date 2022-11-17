8c792bf1230e4bcf173230ba4b613d85f8ae53e6
DerzoGit <journetalexandre.contact@gmail.com>
2022-11-17 15:39:31 +0100

    users index

--- frontend/src/views/admin/users/UserIndex.vue
+++ frontend/src/views/admin/users/UserIndex.vue
@@ -1,6 +1,26 @@
 <template>
     <div>
-        User Index works
+        Liste des utilisateurs ({{ userCount }})
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nom</th>
+                    <th>Prénom</th>
+                    <th>Email</th>
+                    <th>Création</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(user, index) in users" :key="user.id">
+                    <td>{{ user.id }}</td>
+                    <td>{{ user.nom }}</td>
+                    <td>{{ user.prenom }}</td>
+                    <td>{{ user.email }}</td>
+                    <td>{{ dateFormat[index] }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -9,10 +29,26 @@ import { userService } from "@/_services/user.service"
 
 export default {
     name: "UserIndex",
+    data() {
+        return {
+            users: []
+        }
+    },
     mounted() {
         userService.getAllUsers()
-            .then(res => console.log(res.data.data))
+            .then(res => {
+                console.log(res.data.data)
+                this.users = res.data.data
+            })
             .catch(err => console.log(err))
+    },
+    computed: {
+        userCount() {
+            return (this.users.length == 0) ? "Aucun utilisateur" : (this.users.length == 1 ) ? `${ this.users.length } utilisateur` : `${ this.users.length } utilisateurs`
+        },
+        dateFormat() {
+            return this.users.map(user => user.createdAt.split("T")[0].split("-").reverse().join("/"))
+        }
     }
 }
 </script>
