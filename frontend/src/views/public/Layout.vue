51e7ae128915a659e8cd848162e2455288b9dea6
DerzoGit <journetalexandre.contact@gmail.com>
2022-11-23 17:01:26 +0100

    test store

--- frontend/src/views/public/Layout.vue
+++ frontend/src/views/public/Layout.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="public">
+        {{ getJane + " " + getUser(1) }}
+        <button @click="change">Changer Jane</button>
+        <button @click="action">Modification Jane</button>
         <PublicNav/>
         <router-view/>
     </div>
@@ -7,11 +10,27 @@
 
 <script>
 import PublicNav from "@/components/PublicNav"
+import { mapActions, mapGetters, mapMutations } from "vuex"
 
 export default {
     name: "PublicLayout",
     components: {
         PublicNav
+    },
+    computed: {
+        ...mapGetters(["getJane", "getUser"])
+    },
+    methods: {
+        ...mapMutations(["changeJane"]),
+        ...mapActions(["modify"]),
+        change() {
+            // this.$store.commit("changeJane", "Jeanne")
+            this.changeJane("Jaeeeene")
+        },
+        action() {
+            // this.$store.dispatch("modify")
+            this.modify("Jayne")
+        }
     }
 }
 </script>
