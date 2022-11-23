29887a850a4ce269c1c3a8791bf09c00bd97cc5e
DerzoGit <journetalexandre.contact@gmail.com>
2022-11-23 17:01:17 +0100

    test store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,12 +2,30 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    infoState: "testState",
+    users: [
+      { id: 0, name: "Jane" },
+      { id: 1, name: "Jon" }
+    ]
   },
   getters: {
+    getJane(state) {
+      return state.users[0].name
+    },
+    getUser: (state) => (id) => {
+      return state.users[id].name
+    }
   },
   mutations: {
+    changeJane(state, value) {
+      state.users[0].name = value
+    }
   },
   actions: {
+    modify(context, value) {
+      console.log(context)
+      context.commit("changeJane", value)
+    }
   },
   modules: {
   }
