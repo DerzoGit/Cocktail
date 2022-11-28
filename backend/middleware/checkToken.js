9a7c09975af2dffeff2c52259a4c9460c7ffa302
DerzoGit <journetalexandre.contact@gmail.com>
2022-11-28 08:34:56 +0100

    comment checkToken

--- backend/middleware/checkToken.js
+++ backend/middleware/checkToken.js
@@ -1,15 +1,19 @@
+// Import des modules nécessaires
 const jwt = require("jsonwebtoken")
 
+// Extraction du token
 const extractBearer = authorization => {
     if(typeof authorization !== "string") {
         return false
     }
 
+    // Isolation du token
     const matches = authorization.match(/(bearer)\s+(\S+)/i)
 
     return matches && matches[2]
 }
 
+// Vérification de la présence du token
 const checkTokenMiddleware = (req, res, next) => {
     const token = req.headers.authorization && extractBearer(req.headers.authorization)
 
@@ -17,6 +21,7 @@ const checkTokenMiddleware = (req, res, next) => {
         return res.status(401).json({ message: "What are you trying to do ?" })
     }
 
+    // Vérification de la validité du token
     jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
         if(err) {
             return res.status(401).json({ message: "Bad token" })
