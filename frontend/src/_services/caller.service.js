bee1a9941f86892ea6e8f873f44758c23e79f98c
DerzoGit <journetalexandre.contact@gmail.com>
2022-11-28 14:07:16 +0100

    Comment caller.service

--- frontend/src/_services/caller.service.js
+++ frontend/src/_services/caller.service.js
@@ -1,3 +1,4 @@
+// Import des modules nécessaires
 import axios from "axios"
 import { accountService } from "./account.service"
 import router from "@/router"
@@ -7,14 +8,18 @@ const Axios = axios.create({
     baseURL: "http://localhost:3000"
 })
 
+// Interceptor pour injection token
 Axios.interceptors.request.use(request => {
+
+    // Si connecté, ajoute le token dans l'entête
     if(accountService.isLogged()) {
-        request.headers.Authorization = "Bearer " +accountService.getToken() 
+        request.headers.Authorization = "Bearer " + accountService.getToken() 
     }
 
     return request
 })
 
+// Interceptor pour réponse de l'API
 Axios.interceptors.response.use(response => {
     return response
 }, error => {
@@ -28,6 +33,7 @@ Axios.interceptors.response.use(response => {
             accountService.logout()
             router.push("/login")
         } else {
+            // API error
             store.commit("displayNotif", { display: true, message: error.response.data.message })
             return Promise.reject(error)
         }
