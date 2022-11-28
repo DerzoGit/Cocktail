296877ddeb4adda10703d51ed04291eff99ce404
DerzoGit <journetalexandre.contact@gmail.com>
2022-11-28 08:26:36 +0100

    Comment route.user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,15 +1,18 @@
+// Import des modules nécessaires
 const express = require("express")
 const userCtrl = require("../controllers/user")
 
+// Récupération du routeur d'express
 let router = express.Router()
 
+// Middleware logger date des requêtes
 router.use((req, res, next) => {
     const event = new Date()
     console.log("USER Time:", event.toString())
     next()
 })
 
-
+// Routage User
 router.get("/", userCtrl.getAllUsers)
 router.get("/:id", userCtrl.getUser)
 router.patch("/:id", userCtrl.updateUser)
